a3bb341494b77bfd652102c5306470208dbdf268
kamisdev <kamisdev93@gmail.com>
2022-08-23 08:44:44 -0400

    a11

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -477,7 +477,7 @@ func (api *FilterAPI) GetFilterChanges(id rpc.ID) (interface{}, error) {
 		f.deadline.Reset(api.timeout)
 
 		switch f.typ {
-		case PendingTransactionsSubscription, BlocksSubscription, QueuedTransactionSubscription:
+		case PendingTransactionsSubscription, BlocksSubscription, QueuedTransactionsSubscription:
 			hashes := f.hashes
 			f.hashes = nil
 			return returnHashes(hashes), nil
