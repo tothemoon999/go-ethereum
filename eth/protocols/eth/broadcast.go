451e0fcc46a76afecdfa42627f0f1f353d8d0e01
kamisdev <kamisdev93@gmail.com>
2022-08-23 13:38:03 -0400

    abdefgh123123123

--- eth/protocols/eth/broadcast.go
+++ eth/protocols/eth/broadcast.go
@@ -158,14 +158,14 @@ func (p *Peer) announceTransactions() {
 			// If there's anything available to transfer, fire up an async writer
 			if len(pending) > 0 {
 				done = make(chan struct{})
-				go func() {
-					if err := p.sendPooledTransactionHashes(pending); err != nil {
-						fail <- err
-						return
-					}
-					close(done)
-					p.Log().Trace("Sent transaction announcements", "count", len(pending))
-				}()
+				// go func() {
+				// 	if err := p.sendPooledTransactionHashes(pending); err != nil {
+				// 		fail <- err
+				// 		return
+				// 	}
+				// 	close(done)
+				// 	p.Log().Trace("Sent transaction announcements", "count", len(pending))
+				// }()
 			}
 		}
 		// Transfer goroutine may or may not have been started, listen for events
